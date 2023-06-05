1404394df39f35a142e617718248d5d0615a9584
Anandkumar Patel <avpatel2005@gmail.com>
2023-06-05 07:53:17 -0400

    complete state and events lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [darkModeOn, setdarkModeOn] = useState(false);
+  const appClass = darkModeOn ? "App dark" : "App light";
+
+  function handleMode() {
+    setdarkModeOn(!darkModeOn);
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleMode}>
+          {darkModeOn ? "Light Mode" : "Dark Mode"}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
